4f42d6d16dfd810fc0dac7ec46296c65924ec404
yannick <himynameisyannick@gmail.com>
2023-11-26 21:40:24 +0000

    Project Section uncomplete - need working

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -2,14 +2,15 @@ import '../styles/App.css';
 
 function About() {
   return (
-    <div className='about'>
+    <div className='content-wrap'>
+      <div className='title'>
         <h6>ABOUT</h6>
+      </div>
 
         <p>As a front-end developer, I merge technology with aesthetics, drawing inspiration from art and minimalism. Proficient in HTML, CSS, and JavaScript, I create user-friendly interfaces with meticulous attention to detail.</p>
         <p>Guided by minimalistic elegance, each design element serves a purpose. Staying current with the latest front-end technologies, I constantly refine my craft.</p> 
         <p>Beyond coding, I explore design trends and emerging technologies, dedicated to exceeding client and user expectations.</p>
         <p>My journey fuses technological expertise, artistic inspiration, and a commitment to impactful digital experiences.</p>
-
     </div>
   );
 }
--- my-app/src/components/Work.js
+++ my-app/src/components/Work.js
@@ -2,15 +2,24 @@ import '../styles/App.css';
 
 function Work() {
   return (
-    <div className='about'>
+    <div className='content-wrap'>
+      <div className='title'>
         <h6>Work</h6>
+        </div>
 
-        <div className='footer'>
+        <div className='project'>
+          <div className='project-image'>
+          </div>
+          <div className='project-content'>
+          </div>
+        </div>
+
+          <div className='footer'>
             <p className="info">Loosely designed in 
-          <a href="https://www.figma.com/" target="_blank" rel="noreferrer">&nbsp;Figma&nbsp;</a>and coded in 
-          <a href="https://code.visualstudio.com/" target="_blank" rel="noreferrer">&nbsp;Visual Studio Code.&nbsp;</a>Built with
-          <a href="https://www.gatsbyjs.com/" target="_blank" rel="noreferrer">&nbsp;Gatsby.js&nbsp;</a>and deployed with 
-          <a href="https://www.netlify.com/" target="_blank" rel="noreferrer">&nbsp;Netlify.&nbsp;</a></p>
+            <a href="https://www.figma.com/" target="_blank" rel="noreferrer">&nbsp;Figma&nbsp;</a>and coded in 
+            <a href="https://code.visualstudio.com/" target="_blank" rel="noreferrer">&nbsp;Visual Studio Code.&nbsp;</a>Built with
+            <a href="https://react.dev/" target="_blank" rel="noreferrer">&nbsp;React.js&nbsp;</a>and deployed with 
+            <a href="https://www.netlify.com/" target="_blank" rel="noreferrer">&nbsp;Netlify.&nbsp;</a></p>
           </div>
     </div>
   );
