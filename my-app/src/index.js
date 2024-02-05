5318e24f1fbf7ea578e7ddb13ec7564afdca5765
Yannick Lahti <ylahti6@gmail.com>
2024-02-05 18:50:10 +0000

    Revamping the personal page, first save

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,26 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import "./styles/variable.css";
-import App from "./App";
-import Content from "./Content";
-import About from "./Sections/About";
-import Work from "./Sections/Work";
-import Writing from "./Sections/Writing";
-import Footer from "./Sections/Footer";
+import "./styles/global.css";
 import reportWebVitals from "./reportWebVitals";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div className="wrap">
-      <App />
-      <Content>
-        <About />
-        <Work />
-        {/* <Writing /> */}
-        <Footer />
-      </Content>
+      <div className="container-one">
+        <App />
+      </div>
+      <div className="container-two">
+
+      </div>
     </div>
   </React.StrictMode>
 );
